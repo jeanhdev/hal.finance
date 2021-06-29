e570f19ea0d99ff94bb49fa1615b6f475eadc9cb
Jean Hirtz <51744148+jeadev@users.noreply.github.com>
2021-06-29 22:03:02 +0200

    changes

--- pages/api/v1/eth.ts
+++ pages/api/v1/eth.ts
@@ -6,19 +6,44 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const chainId = 1;
   // We'll need to do error handling on each requests, and make multiple axios promises at once instead of 3 calls
   const daily_portfolio = await axios.get(
-    `https://api.covalenthq.com/v1/${chainId}/address/${address}/portfolio_v2/?key=${process.env.COVALENT_API_KEY}`
+    `https://api.covalenthq.com/v1/${chainId}/address/${address}/portfolio_v2/?key=${
+      process.env.COVALENT_API_KEY
+    }&quote-currency=${`USD`}`
   );
+
+  const portfolioData = await daily_portfolio.data;
+
+  const portfolioValue = (items: any) => {
+    let portfolioDailyValue = new Array(31).fill(0);
+    for (let i = 0; i < items.length; i++) {
+      const tokenHoldings = items[i].holdings;
+      tokenHoldings.forEach((dailyHolding, idx) => {
+        portfolioDailyValue[idx] += dailyHolding.close.quote;
+      });
+    }
+    return portfolioDailyValue;
+  };
+
+  const portfolioDays = portfolioData.items[0].holdings.map((tokenHolding: any) => tokenHolding.timestamp);
+  const portfolioValues = portfolioValue(portfolioData.items);
+  const formattedPortfolioData = [];
+  for (let i = 0; i < portfolioDays.length; i++) {
+    formattedPortfolioData.push({
+      date: portfolioDays[i],
+      close: portfolioValues[i],
+    });
+  }
 
   const current_holdings = await axios.get(
     `https://api.covalenthq.com/v1/${chainId}/address/${address}/balances_v2/?key=${process.env.COVALENT_API_KEY}`
   );
 
   const latest_transactions = await axios.get(
-    `https://api.covalenthq.com/v1/${chainId}/address/${address}/transactions_v2/?key=${process.env.COVALENT_API_KEY}`
+    `https://api.covalenthq.com/v1/${chainId}/address/${address}/transactions_v2/?key=${process.env.COVALENT_API_KEY}&quote-currency=USD`
   );
 
   const data = {
-    daily_portfolio: daily_portfolio.data,
+    daily_portfolio: formattedPortfolioData.reverse(),
     current_holdings: current_holdings.data.data,
     latest_transactions: latest_transactions.data.data,
   };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,212 @@
-import { Web3Provider } from "@ethersproject/providers";
-import { useWeb3React } from "@web3-react/core";
-import { GetServerSideProps } from "next";
-import useSWR from "swr";
-import { apiFetcher } from "../utils/fetcher";
-import { injectedConnector } from "./_app";
+import React, { useMemo, useCallback, useState, PureComponent } from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  Line,
+  Tooltip,
+  CartesianGrid,
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
+  LineChart,
+} from "recharts";
+import { TrendingUpIcon, TrendingDownIcon, ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/outline";
+import numeral from "numeral";
+import moment from "moment";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function Home({}) {
   // active will be dynamically set via web3 address fetching
-  const active = true;
-  const address = "YOUR_ETH_ACCOUNT_ADDRESS";
-  const { data, error } = useSWR(`/api/v1/eth?address=${address}`, apiFetcher);
+  const active = false;
+  const address = "0xB1c0d16894e6D3B4C6eE0d40f28Bd1338e996AbD";
+  const [portfolioData, setPorfolioData] = useState([]);
+  const [currentHoldingsData, setCurrentHoldingsData] = useState([]);
+  const [appLoading, setAppLoading] = useState(true);
+  const [btnLoading, setBtnLoading] = useState(false);
+
+  const fetchInvestorData = () => {
+    axios.get(`/api/v1/eth?address=${address}`).then((res) => {
+      const resData = res.data;
+      setPorfolioData(resData.daily_portfolio);
+      setCurrentHoldingsData(resData.current_holdings);
+      setAppLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    fetchInvestorData();
+  }, []);
 
   return (
-    <div>
-      {/* <div>ChainId: {chainId}</div>
-      <div>Account: {account}</div> */}
-
-      <div className='container mx-auto my-3'>
-        <div className='grid place-items-center gap-2'>
-          <h1 className='text-center text-4xl font-bold'>Hal - Ethereum</h1>
-          {active ? (
-            <div>Address: {address}</div>
-          ) : (
-            <a className='px-4 py-2 bg-blue-400 text-white rounded-lg' href='#' onClick={() => null}>
-              Connect your MetaMask 🦊
-            </a>
-          )}
+    <div className='container mx-auto my-3'>
+      <div className='grid place-items-center gap-2 mb-6'>
+        <h1 className='mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl mb-3'>
+          Hal.finance - alpha
+        </h1>
+        <div className='flex flex-row gap-3 items-center'>
+          <div>
+            <input
+              className='shadow appearance-none border rounded w-full px-5 py-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              id='address'
+              type='text'
+              placeholder='Address'
+            />
+          </div>
+          <a
+            className='inline-block px-5 py-3 rounded-lg transform transition bg-green-500 hover:bg-green-500 hover:-translate-y-0.5 focus:ring-green-600 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600  font-semibold text-sm text-white shadow-lg sm:text-base'
+            href='#'
+          >
+            Submit
+          </a>
         </div>
-        <div className='flex flex-col gap-12'>
-          {data && !error && (
-            <>
-              <div className='text-2xl font-semibold'>Portfolio over time</div>
-              {JSON.stringify(data.daily_portfolio)}
-              <div className='text-2xl font-semibold'>Current holdings</div>
-              {JSON.stringify(data.current_holdings)}
-              <div className='text-2xl font-semibold'>Latest transactions</div>
-              {JSON.stringify(data.latest_transactions)}
-            </>
-          )}
-        </div>
+      </div>
+      <div className='flex flex-col gap-12'>
+        {!appLoading && (
+          <>
+            <Portfolio portfolioData={portfolioData} />
+            <Holdings currentHoldingsData={currentHoldingsData} />
+          </>
+        )}
       </div>
     </div>
   );
 }
+
+const computeChange = (today: any, yesterday: any) => {
+  const change = (((today - yesterday) / today) * 100) as any;
+  return change;
+};
+
+const Portfolio = ({ portfolioData }) => {
+  const todayPortfolioValue = {
+    close: portfolioData[portfolioData.length - 1].close,
+    change: computeChange(portfolioData[portfolioData.length - 1].close, portfolioData[portfolioData.length - 2].close),
+  };
+  const [today, setToday] = useState(portfolioData[portfolioData.length - 1]);
+  const [portfolioValue, setPortfolioValue] = useState(todayPortfolioValue);
+
+  useEffect(() => {}, [today]);
+
+  const handlePortfolioValue = (dayPortfolio: any) => {
+    // Value of today
+    const todayDate = dayPortfolio.date;
+    const todayValue = dayPortfolio.close;
+    const todayPosition = portfolioData.filter((day) => day.date === todayDate)[0];
+    // Find yesterday
+    const yesterdayPosition = portfolioData.indexOf(todayPosition) - 1;
+    const yesterdayValue = portfolioData[yesterdayPosition].close;
+    // Compute the change
+    const change = computeChange(todayValue, yesterdayValue);
+    setPortfolioValue({
+      close: todayValue,
+      change: change,
+    });
+  };
+
+  return (
+    <div className='flex flex-col gap-6  bg-white p-8 rounded-lg shadow-md'>
+      <div className='flex flex-row justify-between items-center'>
+        <h2 className='text-2xl font-medium leading-6 text-gray-900'>Portfolio</h2>
+        <div className='flex flex-row items-center gap-2'>
+          {portfolioValue.change > 0 ? (
+            <div className='flex flex-row items-center text-green-500'>
+              <ArrowUpIcon className='h-6 w-6 ' />
+              <h3 className='font-semibold text-xl'>{numeral(portfolioValue.change).format("(0.00)")} %</h3>
+            </div>
+          ) : (
+            <div className='flex flex-row items-center text-red-500'>
+              <ArrowDownIcon className='h-5 w-5' />
+              <h3 className='font-semibold text-xl'>{numeral(portfolioValue.change).format("(0.00)")} %</h3>
+            </div>
+          )}
+          <h3 className='font-semibold text-xl'>{numeral(portfolioValue).format("$(0.00 a)")}</h3>
+        </div>
+      </div>
+      <div style={{ height: "30vh", width: "100%" }}>
+        <ResponsiveContainer width='100%' height='100%'>
+          <LineChart data={portfolioData}>
+            <Line type='monotone' dataKey='close' stroke='#10B981' dot={null} />
+            <XAxis dataKey='date' tickFormatter={(date) => `${moment(date).format("L")}`} interval='preserveEnd' />
+            <YAxis dataKey='close' tickFormatter={(close) => `$${numeral(close).format("(0 a)")}`} />
+            <Tooltip
+              cursor={false}
+              content={<CustomTooltip handlePortfolioValue={(data) => handlePortfolioValue(data)} />}
+            />
+            {/* <CartesianGrid /> */}
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+const CustomTooltip = ({ active, payload, handlePortfolioValue }: any) => {
+  if (active) {
+    handlePortfolioValue(payload[0].payload);
+    return (
+      // <div className='p-3 bg-blue-400'>
+      //   <h4>{value}</h4>
+      // </div>
+      null
+    );
+  }
+  return null;
+};
+
+const Holdings = ({ currentHoldingsData }) => {
+  const tokens = currentHoldingsData.items;
+  const totalPortfolioValue = tokens.map((token: any) => token.quote).reduce((a, b) => a + b);
+
+  const getTokenWeight = (tokenQuote: number) => {
+    const tokenWeigth = tokenQuote / totalPortfolioValue;
+    return tokenWeigth.toString();
+  };
+
+  return (
+    <>
+      <div className='flex flex-col gap-6 bg-white p-8 rounded-lg shadow-md'>
+        <h2 className='text-2xl font-medium leading-6 text-gray-900'>Current holdings</h2>
+        <div style={{ height: "20vh" }}>
+          <div className='grid grid-flow-col grid-cols-2'>
+            <div id='holdings-table w-full'>
+              <table className='table-auto w-full'>
+                <thead>
+                  <tr className='text-left'>
+                    <th align='center'>Token</th>
+                    <th align='center'>Value</th>
+                    <th align='center'>Weight</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tokens.map((token: any) => (
+                    <tr>
+                      <td>
+                        {token.contract_name} ({token.contract_ticker_symbol})
+                      </td>
+                      <td align='right'>{numeral(token.quote).format("(0.00 a)")}</td>
+                      <td align='right'>{numeral(getTokenWeight(token.quote)).format("0.00 %")}</td>
+                    </tr>
+                  ))}
+                </tbody>
+                <tfoot className='border-t-2 border-gray-500'>
+                  <tr>
+                    <td>Portfolio</td>
+                    <td align='right'>{numeral(totalPortfolioValue).format("(0.00 a)")}</td>
+                    <td align='right'>100 %</td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+            <div id='holdings-pie w-full'>
+              <h3>pie chart</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
